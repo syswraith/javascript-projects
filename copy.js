c29d776169d8b8205cf3f94b69630c8d57b18a4f
Syswraith <77088155+syswraith@users.noreply.github.com>
2023-01-11 21:40:01 +0530

    Update copy.js

--- copy.js
+++ copy.js
@@ -1,6 +1,6 @@
 // use CSS.escape() to escape characters that need escaping
-let str = document.querySelector(/*add selector path here*/).innerText;
-const google = "https://www.google.com/search?q=";
+str = document.querySelector(/*add selector path here*/).innerText;
+google = "https://www.google.com/search?q=";
 splitStr = str.split(' ');
 splitFormattedStr = splitStr.map((currentValue) => {
     if (/\d/.test(currentValue)) { return `"${currentValue}"` }
