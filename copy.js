1f11ba5649b245565e2a7c1df19a5c5d2f597ca3
Syswraith <77088155+syswraith@users.noreply.github.com>
2023-01-12 16:36:58 +0530

    Update copy.js

--- copy.js
+++ copy.js
@@ -9,3 +9,6 @@ splitFormattedStr = splitStr.map((currentValue) => {
 });
 let query = google + splitFormattedStr.join('+');
 window.open(query);
+
+// bookmarklet - add the selector and copy paste it in the url section of a bookmark:
+/* javascript:{selector="add selector here";str=document.querySelector(selector).innerText;google="https://www.google.com/search?q=";splitStr=str.split(' ');splitFormattedStr=splitStr.map((currentValue)=>{if (/\d/.test(currentValue)){return `"${currentValue}"`}else{return currentValue}});let query=google + splitFormattedStr.join('+');window.open(query);} */
