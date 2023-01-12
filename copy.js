78002da38c55e4bdfe59620088d0930edb594bea
Syswraith <77088155+syswraith@users.noreply.github.com>
2023-01-12 17:08:44 +0530

    A little help for you in your next online test

--- copy.js
+++ copy.js
@@ -9,6 +9,5 @@ splitFormattedStr = splitStr.map((currentValue) => {
 });
 let query = google + splitFormattedStr.join('+');
 window.open(query);
-
 // bookmarklet - add the selector and copy paste it in the url section of a bookmark:
 /* javascript:{selector="add selector here";str=document.querySelector(selector).innerText;google="https://www.google.com/search?q=";splitStr=str.split(%27 %27);splitFormattedStr=splitStr.map((currentValue)=>{if (/\d/.test(currentValue)){return %60"${currentValue}"%60}else{return currentValue}});let query=google + splitFormattedStr.join(%27+%27);window.open(query); console.log('find the original at %chttps://github.com/syswraith/javascript-projects/edit/main/copy.js','color:#00ff00;font-size:20px;');} */
